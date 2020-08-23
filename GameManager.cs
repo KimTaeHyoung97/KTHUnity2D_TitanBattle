ca0405617b2bf65f017cde5643cbb80b6bb9b65e
KimTaeHyoung97 <69450119+KimTaeHyoung97@users.noreply.github.com>
2020-08-23 21:35:58 +0900

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class GameManager : MonoBehaviour
+{
+    Text gametitleText, gamestartText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gametitleText = GameObject.Find("GameTitle").GetComponent<Text>();
+        gamestartText = GameObject.Find("GameStartText").GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickStart()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
+
